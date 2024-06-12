74eda5b27947b34370ffbe102559e78d5992e082
Fluorine <127977090+FluorineV@users.noreply.github.com>
2024-06-12 17:26:54 +0700

    5.0.7

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         florr.io | ReporterTest
 // @namespace    Furaken
-// @version      5.0.6
+// @version      5.0.7
 // @description  $$$$$$$$$
 // @author       Furaken
 // @match        https://florr.io/*
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 var allAvailableServers = 6,
-    versionScript = "5.0.6",
+    versionScript = "5.0.7",
     uniqueSpawnMessages = {
         "Cactus": "A tower of thorns rises from the sands...",
         "Hel Beetle": "You sense ominous vibrations coming from a different realm...",
@@ -78,11 +78,23 @@ const Tracker = new class {
         Super.open("POST", "https://discord.com/api/webhooks/1233383292615655486/SQoOrPYliiJi1UG8hLUig1JnzKmu636-VkPGXvzBXHQ7tV-5LPN0zzY-4s6-gdFLmmJE");
         Super.setRequestHeader('Content-type', 'application/json');
 
+        const loggerSpawn = new XMLHttpRequest();
+        loggerSpawn.open("POST", "https://discord.com/api/webhooks/1205022085458694144/JNbKfgDp0S0O9oGmOffhT7MjsrRz7AJf95uHMHIYH3J1ph9XELXQ_HhvFxiGFdxbK2z8");
+        loggerSpawn.setRequestHeader('Content-type', 'application/json');
+
+        const loggerDeath = new XMLHttpRequest();
+        loggerDeath.open("POST", "https://discord.com/api/webhooks/1205022959853641778/ZGwLN9DDfFwJNswhpT9VRlxKGvba3Aqnm6kQ6bFXVTcFhmmL8rizRp4ege91Cx5AUmZS");
+        loggerDeath.setRequestHeader('Content-type', 'application/json');
+
         const loggerCraft = new XMLHttpRequest();
         loggerCraft.open("POST", "https://discord.com/api/webhooks/1205022798712672307/pGIe2UzMSqwopDI_gHpudSmC2oBc0vW7huWwptAPwC9Rp52CvYJCcAkR-fkwKXqgyXHf");
         loggerCraft.setRequestHeader('Content-type', 'application/json');
 
-        return {Super, loggerCraft}
+        const Logger = new XMLHttpRequest();
+        Logger.open("POST", "https://discord.com/api/webhooks/1205023134223433769/UJBpKmPVwpkbJ-_KdS4Elkf8AHmnz15XgzsLfR6ntaF3ESw30SzxfGprza9cOKDstORK");
+        Logger.setRequestHeader('Content-type', 'application/json');
+
+        return {Super, loggerSpawn, loggerDeath, loggerCraft, Logger}
     }
     TrackerNotifier(content, thisMobName, thisMobRarity, thisPlayer, thisTextColor, type) {
         if (thisTextColor == "#ffffff" || thisTextColor == "#000000") return
@@ -109,9 +121,37 @@ const Tracker = new class {
                     }
                 }],
             }));
+            Tracker.XHR().Logger.send(JSON.stringify({
+                content: "```js\n" + JSON.stringify({
+                    "type": "Report",
+                    "region": thisServerName,
+                    "map": thisCurrentMap,
+                    "rarity": thisMobRarity,
+                    "mob": thisMobName,
+                    "time": Math.floor(Date.now() / 1000),
+                    "version": versionScript
+                }, null, 4) + "```",
+            }));
+            if (thisMobRarity == "Super") {
+                Tracker.XHR().loggerSpawn.send(JSON.stringify({
+                    username: "Reporter - " + versionScript,
+                    content: `${thisServerName}: ${thisMobRarity} ${thisMobName}\n\`${Math.floor(Date.now() / 1000)}\`\n<t:${Math.floor(Date.now() / 1000)}:R>\n_\n_`
+                }));
+            }
         } else if (type == "Death") {
             if (thisMobName != lastMobNameKillMsgReportedToTracker) return
             lastMobNameKillMsgReportedToTracker = ""
+            Tracker.XHR().Logger.send(JSON.stringify({
+                content: "```js\n" + JSON.stringify({
+                    "type": "Death",
+                    "region": thisServerName,
+                    "map": thisCurrentMap,
+                    "rarity": thisMobRarity,
+                    "mob": thisMobName,
+                    "time": Math.floor(Date.now() / 1000),
+                    "version": versionScript
+                }, null, 4) + "```",
+            }));
             if ((thisMobRarity == "Super")) {
                 Tracker.XHR().Super.send(JSON.stringify({
                     embeds: [{
@@ -123,9 +163,27 @@ const Tracker = new class {
                     }],
                 }));
             }
+            if (thisMobRarity == "Super") {
+                Tracker.XHR().loggerDeath.send(JSON.stringify({
+                    username: "Reporter - " + versionScript,
+                    content: `${content.replaceAll("_", "\\_")}\n\`${Math.floor(Date.now() / 1000)}\`\n<t:${Math.floor(Date.now() / 1000)}:R>\n_\n_`
+                }));
+            }
         } else if (type == "Craft") {
             if (thisMobName != lastPetalCraftReportedToTracker) return
             lastPetalCraftReportedToTracker = ""
+            Tracker.XHR().Logger.send(JSON.stringify({
+                content: "```js\n" + JSON.stringify({
+                    "type": "Craft",
+                    "region": thisServerName,
+                    "map": thisCurrentMap,
+                    "rarity": thisMobRarity,
+                    "petal": thisMobName,
+                    "player": thisPlayer,
+                    "time": Math.floor(Date.now() / 1000),
+                    "version": versionScript
+                }, null, 4) + "```",
+            }));
             if (thisMobRarity == "Super") {
                 Tracker.XHR().Super.send(JSON.stringify({
                     content: discordCraftRoleId,
